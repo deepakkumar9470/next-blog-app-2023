a2918cd2ac5c059a786e77d9b2afab68667e6496
deepakkumar9470 <dk397787@gmail.com>
2023-07-04 09:06:05 +0530

    next blog v1

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -9,7 +9,7 @@ export const metadata = {
 const Contact = () => {
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>Let's keep in touch</h1>
+      <h1 className={styles.title}>Please keep in touch with us</h1>
     
     <div className={styles.content}>
          <div className={styles.imgContainer}>
